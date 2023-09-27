2cf8e8f28011a60b886dfae4e269d1b727112d9a
Petter <petter.gustafsson@hotmail.com>
2023-09-27 19:56:03 +0200

    im starting with this

--- App.js
+++ App.js
@@ -4,19 +4,12 @@ import { Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <Text style={styles.headline}>This is a headline!!!!</Text>
+      <View style={styles.headlineContainer}>
+        <Text style={styles.headline}>Lets make a shoppinglist!</Text>
+        <Text style={styles.subtitle}>Add to the list</Text>
       </View>
-        <View style={styles.textView}>
-        <Text style={styles.text}>
-          Now we are gonna builde something awesome! 
-        </Text>
-        <Text style={styles.text}>
-          Yay! I'm ready for you. Trying to create a Pull request now.
-        </Text>
-        <Text style={styles.text2}>
-          What do you want to do?
-        </Text>
+
+        <View style={styles.buttonContainer}>
         <Button title="Press"
         color='purple'
         onPress={() => ("")}/>
@@ -29,31 +22,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "lightgreen",
+    //backgroundColor: "lightgreen",
     alignItems: "center",
     justifyContent: "center",
   },
-  textView:{
+  headlineContainer:{
 flex: 1,
-alignContent: 'center',
-justifyContent: 'center'
-  },
-  text: {
-    color: "red",
-    fontSize: 20,
-    alignContent: 'center'
-    
+alignItems: 'center'
   },
   headline: {
     fontSize: 30,
     color: "blue",
     padding: 20
   },
-  text2: {
-    padding: 20,
-    color: "green",
-    fontSize: 30,
-    alignContent: 'center',
+  subtitle: {
+    fontSize: 20,
+    color: "blue",
+    padding: 10,
     
   },
+  buttonContainer: {
+    paddingBottom: 10
+  }
 });
